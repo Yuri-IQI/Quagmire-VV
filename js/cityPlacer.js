f0df6722c2a53ae3807fc2e99cab8f32e7551b67
Yuri <anthony.yuri@souunit.com.br>
2023-11-17 19:43:57 -0300

    petty lines in the area

--- js/cityPlacer.js
+++ js/cityPlacer.js
@@ -12,11 +12,35 @@ window.onload = function() {
         var img = document.createElement("img");
         img.src = imageUrl;
         img.style.position = "absolute";
-        img.style.left = coordinatesArray[i][0] + "px";
+        img.style.left = coordinatesArray[i][0] - 5 + "px";
         img.style.top = coordinatesArray[i][1] + "px";
         
         iconsDiv.appendChild(img);
       }
+    });
+
+    fetch('http://127.0.0.1:5000/get_revised_connections')
+    .then(response => response.json())
+    .then(data => {
+        var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        var routes = document.querySelector('.routes');
+        svg.setAttribute("width", "1340");
+        svg.setAttribute("height", "900");
+        routes.appendChild(svg);
+
+        for (let i = 0; i < data.length; i++) {
+            let pointA = data[i][0][1];
+            let pointB = data[i][1][1];
+
+            var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+            line.setAttribute("x1", pointA[0]);
+            line.setAttribute("y1", pointA[1]);
+            line.setAttribute("x2", pointB[0]);
+            line.setAttribute("y2", pointB[1]);
+            line.setAttribute("stroke-width", "2");
+            line.setAttribute("stroke", "black");
+            svg.appendChild(line);
+        }
     });
 }
 
--- mire.py
+++ mire.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import psycopg2
+import math
 
 app = Flask(__name__)
 CORS(app)
@@ -20,15 +21,45 @@ class Fetcher:
         cursor.execute(query)
         return cursor.fetchall()  
 
+class Routes:
+    def __init__(self):
+        self.cities = []
+        self.connections = []
+        self.revised_connections = []
+    
+    def way(self):
+        for i in range(len(self.cities)):
+            for j in range(i+1, len(self.cities)):
+                self.connections.append([self.cities[i], self.cities[j]])
+        return self.connections
+    
+    def measure(self):
+        for connection in self.connections:
+            A = connection[0][1]
+            B = connection[1][1]
+            length = math.sqrt((B[0] - A[0])**2 + (B[1] - A[1])**2)
+
+            if length < 100:
+                self.revised_connections.append(connection)
+        return self.revised_connections
+
 fetcher = Fetcher()
+path = Routes()
+
 coordenadas = []
 for id, coordenada in fetcher.fetch("SELECT id, coords FROM cities"):
+    path.cities.append([id, coordenada])
     coordenadas.append(coordenada)
-print(coordenadas)
 
 @app.route('/get_coordinates', methods=['GET'])
 def get_coordinates():
     return jsonify(coordenadas)
+
+@app.route('/get_revised_connections', methods=['GET'])
+def get_revised_connections():
+    path.way()
+    revised_connections = path.measure()
+    return jsonify(revised_connections)
 
 if __name__ == '__main__':
     app.run(debug=True)
